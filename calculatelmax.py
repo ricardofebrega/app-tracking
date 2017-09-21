a633e37ff1da7a59b2d5d6e96c6543292ef1e67e
Soichi Hayashi <hayashis@iu.edu>
2017-09-21 19:23:14 -0400

    fixed start.sh to support bigred2

--- calculatelmax.py
+++ calculatelmax.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Fri Aug  4 20:06:19 2017
@@ -9,10 +10,11 @@ import json
 
 with open('config.json') as config_json:
     config = json.load(config_json)
-
+with open(config["dtiinit"]+'/dt6.json') as dt6_json:
+    dt6config = json.load(dt6_json)
 
 #get non0 bvals
-f = open(config["bvals"], 'r')
+f = open(config["dtiinit"]+"/"+dt6config["files"]["alignedDwBvals"], 'r')
 line = f.readline().strip().replace(",", " ")
 bvals = line.split(" ")
 bvals_non0 = filter(lambda v: v != "0", bvals)
@@ -23,9 +25,5 @@ lmax=0
 while (lmax+3)*(lmax+4)/2 <= count:
        lmax+=2
 
-#print "lmax should be ", lmax, "for bvals:", count
 print lmax
 
-#outfile = open('lmax.txt', 'w')
-#outfile.write(str(lmax))
-#outfile.close()
--- start.sh
+++ start.sh
@@ -10,36 +10,14 @@ if [ -z $ENV ]; then export ENV=IUHPC; fi
 #cleanup
 rm -f finished
 
-#find out which environment we are in
-hostname | grep karst > /dev/null
-if [ $? -eq 0 ]; then
-    execenv=karst 
-fi
-echo $HOME | grep -i bigred > /dev/null
-if [ $? -eq 0 ]; then
-    execenv=bigred
-    module load ccm
-fi
-if [ -z "$execenv" ]; then 
-    print "couldn't figure out which environment this is"
-    exit 1
-fi
-echo "seems to be running on $execenv"
-
-###############################################################
-# write .mrtrix.conf 
 # TODO - handle a case where this file already exists
 if [ ! -e $HOME/.mrtrix.conf ]; then
-    echo "NumberOfThreads: 16" > $HOME/.mrtrix.conf
+    echo "NumberOfThreads: 8" > $HOME/.mrtrix.conf
 fi
 
-###############################################################
-# run submit.pbs
-
 OPTS=""
-if [ $execenv == "bigred" ]; then
+if [ $HPC == "BIGRED2" ]; then
     OPTS="-v CCM=1 -l gres=ccm"
 fi
 jobid=`qsub $OPTS $SERVICE_DIR/submit.pbs`
 echo $jobid > jobid
-#curl -s -X POST -H "Content-Type: application/json" -d "{\"status\": \"waiting\", \"progress\": 0, \"msg\":\"Job: $jobid Waiting in PBS queue on $execenv\"}" $PROGRESS_URL > /dev/null
